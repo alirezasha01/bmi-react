291134729033e0698eafca50950aa5a1b018aecc
Alireza <alirezasha01@gmail.com>
2023-04-17 01:54:56 +0430

    Calculate the situation from bmi

--- src/components/RangeInput.jsx
+++ src/components/RangeInput.jsx
@@ -7,6 +7,18 @@ const RangeInput = () => {
     const [ valueWeight, setValueWeight ] = React.useState(80);
     const [ valueHeight , setValueHeight ] = React.useState(150);
     const Result = Math.round(valueWeight / ((valueHeight/100)*(valueHeight/100)))
+    let Bmi="";
+    if(Result < 18){
+        Bmi = "You are underweight"
+    }
+    else if ( 18 <= Result && Result < 25) {
+        Bmi = "You are normal"
+    } 
+    else if (25 <= Result && Result < 40) {
+        Bmi = "You are overweight"
+    } else {
+        Bmi = "You are obese"
+    }
   return (
     <>
     <Container>
@@ -14,13 +26,17 @@ const RangeInput = () => {
             <Col md={2}></Col>
             <Col>
                <h1>BMI Calculator</h1>
-               <Form.Label>Weight</Form.Label>
-               <Form.Range  min={0} max={300} value={valueWeight} onChange={e => setValueWeight(e.target.value)}/>
+               <Form.Label className='form-label'>Weight</Form.Label>
+               <Form.Range  min={1} max={300} value={valueWeight} onChange={e => setValueWeight(e.target.value)}/>
                <h5>{valueWeight}kg</h5>
-               <Form.Label>Height</Form.Label>
-               <Form.Range  min={0} max={300} value={valueHeight} onChange={m => setValueHeight(m.target.value)}/>
+               <Form.Label className='mt-5 form-label'>Height</Form.Label>
+               <Form.Range  min={1} max={300} value={valueHeight} onChange={m => setValueHeight(m.target.value)}/>
                <h5>{valueHeight}cm</h5>
                <h4>Your BMI is : {Result}</h4>
+               <div className='display-grid'>
+               <h4>{Bmi}</h4>
+               </div>
+               
                </Col>
                <Col md={2}></Col>
         </Row>
