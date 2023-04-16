dffe81bbb084151581073c04e69a1a1a99d2d819
Alireza <alirezasha01@gmail.com>
2023-04-17 01:15:28 +0430

    calculate bmi and show this

--- src/components/RangeInput.jsx
+++ src/components/RangeInput.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import {Container, Form  , Row , Col} from 'react-bootstrap';
-
+import './RangeInput.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const RangeInput = () => {
     const [ valueWeight, setValueWeight ] = React.useState(80);
     const [ valueHeight , setValueHeight ] = React.useState(150);
+    const Result = Math.round(valueWeight / ((valueHeight/100)*(valueHeight/100)))
   return (
     <>
     <Container>
         <Row>
             <Col md={2}></Col>
             <Col>
+               <h1>BMI Calculator</h1>
                <Form.Label>Weight</Form.Label>
                <Form.Range  min={0} max={300} value={valueWeight} onChange={e => setValueWeight(e.target.value)}/>
-               <p>{valueWeight}</p>
+               <h5>{valueWeight}kg</h5>
                <Form.Label>Height</Form.Label>
                <Form.Range  min={0} max={300} value={valueHeight} onChange={m => setValueHeight(m.target.value)}/>
-               <p>{valueHeight}</p></Col>
-               <Col md={2}>
-            </Col>
+               <h5>{valueHeight}cm</h5>
+               <h4>Your BMI is : {Result}</h4>
+               </Col>
+               <Col md={2}></Col>
         </Row>
 
     </Container>
